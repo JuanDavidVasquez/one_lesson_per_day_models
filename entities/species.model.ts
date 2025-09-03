6cf83e35b16988dd2f636c8bbae3d3235259f8d9
Juan David <ingjuandavid@hotmail.com>
2025-09-03 07:28:17 -0500

    [FIX] species model

--- entities/species.model.ts
+++ entities/species.model.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from 'typeorm';
 import { Pet } from './pet.model';
 
 
@@ -8,7 +8,16 @@ export class Species {
   id!: number;
 
   @Column({ length: 50, unique: true })
-  name!: string; // perro, gato, conejo, etc.
+  name!: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt!: Date;
+
+  @DeleteDateColumn({ type: 'timestamp', nullable: true })
+  deletedAt!: Date;
 
   @OneToMany(() => Pet, (pet) => pet.species)
   pets!: Pet[];
